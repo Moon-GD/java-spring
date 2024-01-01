a440da44ebead4eb59b45aa8afcf038c0a391384
Moon-GD <king4mun@gmail.com>
2024-01-01 17:59:20 +0900

    [Java Spring] [B/E] 불필요한 import문 제거

--- src/test/java/MoonGD/hellospring/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/MoonGD/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -1,7 +1,6 @@
 package MoonGD.hellospring.repository;
 
 import MoonGD.hellospring.domain.Member;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
